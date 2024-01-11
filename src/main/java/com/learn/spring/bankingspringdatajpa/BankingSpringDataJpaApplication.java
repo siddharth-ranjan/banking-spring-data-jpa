e4c4e92b9bc4a3a064f137449c66e05662060eff
Siddharth Ranjan <siddharthranjan0909@gmail.com>
2024-01-11 21:20:43 +0530

    Initial commit

---
+++ src/main/java/com/learn/spring/bankingspringdatajpa/BankingSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.learn.spring.bankingspringdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingSpringDataJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankingSpringDataJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/learn/spring/bankingspringdatajpa/BankingSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.spring.bankingspringdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingSpringDataJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
